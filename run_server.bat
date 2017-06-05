5501d2231a24c840a077fb8da2a6ecab82d5d01b
ttimt <geroginalam@hotmail.com>
2017-06-05 12:59:52 +0800

    Initial commit

---
+++ run_server.bat
@@ -0,0 +1,1 @@
+php C:\xampp\htdocs\beelikeus\artisan serve
---
+++ webpack.mix.js
@@ -0,0 +1,27 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/assets/js/app.js', 'public/js/temp.js')
+   .sass('resources/assets/sass/app.scss', 'public/css/temp.css');
+   
+mix.styles([
+	'public/css/temp.css',
+	'node_modules/font-awesome/css/font-awesome.min.css',
+	'node_modules/foundation-sites/dist/css/foundation.min.css'
+], 'public/css/app.css');
+
+mix.combine([
+	'node_modules/jquery/dist/jquery.min.js',
+	'public/js/temp.js',
+	'node_modules/foundation-sites/dist/js/foundation.min.js'
+], 'public/js/app.js');
